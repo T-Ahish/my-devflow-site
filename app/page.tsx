88fe87e7836cd24cd7f26da5e7b71e961b88ed5f
T-Ahish <ahishgameking@gmail.com>
2025-11-09 22:35:45 +0530

    Project Setup: Prettier, ESLint and TailwindCSS along with Google Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import "./globals.css";
+
 export const Home = () => {
-  return <h1>Welcome to My Devflow Site!</h1>;
+  return (
+    <>
+      <h1 className="flex-center h1-bold">Welcome to My Devflow Site!</h1>
+      <p className="font-space-grotesk">I am a random Text</p>
+    </>
+  );
 };
 
 export default Home;

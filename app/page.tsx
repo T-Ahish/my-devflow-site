e1bc2c4f81b03ea0d6d21f9977e93240a7e7b658
T-Ahish <ahishgameking@gmail.com>
2025-11-20 10:05:06 +0530

    Initalize ShadCN Module along with its Dropdown Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import "./globals.css";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export const Home = () => {
   return (
     <>
       <h1 className="flex-center h1-bold">Welcome to My Devflow Site!</h1>
       <p className="font-space-grotesk">I am a random Text</p>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
